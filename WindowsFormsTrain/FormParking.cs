b688134dba05e844d411ba47a1bf3a4bc26d722a
RiptizeD <vov4ik.exe@gmail.com>
2020-02-13 22:07:54 +0400

    Лаб №5 Глухов В.А. ПИбд-21

---
+++ WindowsFormsTrain/FormParking.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTrain
+{
+    public partial class FormParking : Form
+    {
+        MultiLevelParking parking;
+        private const int countLevel = 5;
+        FormTrainConfig trainForm;
+
+        public FormParking()
+        {
+            InitializeComponent();
+            parking = new MultiLevelParking(countLevel, pictureBoxParking1.Width,
+            pictureBoxParking1.Height);
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxLevels.SelectedIndex = 0;
+        }
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+            Bitmap bmp = new Bitmap(pictureBoxParking1.Width,
+            pictureBoxParking1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking[listBoxLevels.SelectedIndex].Draw(gr);
+                pictureBoxParking1.Image = bmp;
+            }
+        }
+
+        private void buttonSetLocomotive_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var train = new TrainVehicle(100, 1000, dialog.Color);
+                    int place = parking[listBoxLevels.SelectedIndex] + train;
+                    if (place == -1)
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    Draw();
+                }
+            }
+        }
+
+        private void buttonTakeTrain_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                if (maskedTextBox1.Text != "")
+                {
+                    var train = parking[listBoxLevels.SelectedIndex] -  Convert.ToInt32(maskedTextBox1.Text);
+                    if (train != null)
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxParking1.Width,
+                       pictureBoxTakeTrain.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        train.SetPosition(5, 100, pictureBoxTakeTrain.Width,
+                       pictureBoxTakeTrain.Height);
+                        train.DrawTrain(gr);
+                        pictureBoxTakeTrain.Image = bmp;
+                    }
+                    else
+                    {
+                        Bitmap bmp = new Bitmap(pictureBoxTakeTrain.Width,
+                       pictureBoxTakeTrain.Height);
+                        pictureBoxTakeTrain.Image = bmp;
+                    }
+                    Draw();
+                }
+            }
+        }
+
+        private void buttonSetElecTrain_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var train = new ElecTrain(100, 1000, dialog.Color, dialogDop.Color,
+                       true, true);
+                        int place = parking[listBoxLevels.SelectedIndex] + train;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка",
+                           MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        Draw();
+                    }
+                }
+            }
+
+        }
+
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+       
+        private void AddTrain(ITransport train)
+        {
+            if (train != null && listBoxLevels.SelectedIndex > -1)
+            {
+                int place = parking[listBoxLevels.SelectedIndex] + train;
+                if (place > -1)
+                {
+                    Draw();
+                }
+                else
+                {
+                    MessageBox.Show("Машину не удалось поставить");
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            trainForm = new FormTrainConfig();
+            trainForm.AddEvent(AddTrain);
+            trainForm.Show();
+        }
+    }
+}
---
+++ WindowsFormsTrain/FormTrainConfig.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTrain
+{
+    public partial class FormTrainConfig : Form
+    {
+        ITransport train = null;
+        private event trainDelegate eventAddTrain;
+        public FormTrainConfig()
+        {
+            InitializeComponent();
+            panelBlack.MouseDown += panelColor_MouseDown;
+            panelGold.MouseDown += panelColor_MouseDown;
+            panelGray.MouseDown += panelColor_MouseDown;
+            panelGreen.MouseDown += panelColor_MouseDown;
+            panelRed.MouseDown += panelColor_MouseDown;
+            panelWhite.MouseDown += panelColor_MouseDown;
+            panelGold.MouseDown += panelColor_MouseDown;
+            panelBlue.MouseDown += panelColor_MouseDown;
+            buttonCancel.Click += (object sender, EventArgs e) => { Close(); };
+        }
+
+        private void DrawTrain()
+        {
+            if (train != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxTrainConfig.Width, pictureBoxTrainConfig.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                train.SetPosition(5, 5, pictureBoxTrainConfig.Width, pictureBoxTrainConfig.Height);
+                train.DrawTrain(gr);
+                pictureBoxTrainConfig.Image = bmp;
+            }
+        }
+
+        public void AddEvent(trainDelegate ev)
+        {
+            if (eventAddTrain == null)
+            {
+                eventAddTrain = new trainDelegate(ev);
+            }
+            else
+            {
+                eventAddTrain += ev;
+            }
+        }
+
+        private void panelColor_MouseDown(object sender, MouseEventArgs e)
+        {
+            (sender as Control).DoDragDrop((sender as Control).BackColor,
+           DragDropEffects.Move | DragDropEffects.Copy);
+        }
+
+        private void labelBaseColor_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(Color)))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void labelDopColor_DragDrop(object sender, DragEventArgs e)
+        {
+            if (train != null)
+            {
+                if (train is ElecTrain)
+                {
+                    (train as
+                   ElecTrain).SetDopColor((Color)e.Data.GetData(typeof(Color)));
+                    DrawTrain();
+                }
+            }
+        }
+  
+        private void labelBaseColor_DragDrop(object sender, DragEventArgs e)
+        {
+            if (train != null)
+            {
+                train.SetMainColor((Color)e.Data.GetData(typeof(Color)));
+                DrawTrain();
+            }
+        }
+        private void labelTrain_MouseDown(object sender, MouseEventArgs e)
+        {
+            labelLocomotive.DoDragDrop(labelLocomotive.Text, DragDropEffects.Move | DragDropEffects.Copy);
+        }
+
+        private void labelElecTrain_MouseDown(object sender, MouseEventArgs e)
+        {
+            labelElecTrain.DoDragDrop(labelElecTrain.Text, DragDropEffects.Move |
+           DragDropEffects.Copy);
+        }
+
+        private void panelTrain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void panelTrain_DragDrop(object sender, DragEventArgs e)
+        {
+            switch (e.Data.GetData(DataFormats.Text).ToString())
+            {
+                case "Locomotive":
+                    train = new TrainVehicle(100, 500, Color.White);
+                    break;
+                case "ElecTrain":
+                    train = new ElecTrain(100, 500, Color.White, Color.Black, true, true);
+                    break;
+            }
+            DrawTrain();
+        }
+
+        private void ButtonOk_Click(object sender, EventArgs e)
+        {
+            eventAddTrain?.Invoke(train);
+            Close();
+        }
+    }
+}
