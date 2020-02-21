ce7592e72ea59c07110925c960026639fa0ad58e
RiptizeD <vov4ik.exe@gmail.com>
2020-02-21 11:56:52 +0400

    Лаб №7 Глухов В.А. ПИбд-21

---
+++ WindowsFormsTrain/FormParking.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using NLog;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTrain
+{
+    public partial class FormParking : Form
+    {
+        MultiLevelParking parking;
+        private const int countLevel = 5;
+        private Logger logger;
+        FormTrainConfig trainForm;
+
+        public FormParking()
+        {
+            InitializeComponent();
+            logger = LogManager.GetCurrentClassLogger();
+            parking = new MultiLevelParking(countLevel, pictureBoxParking.Width,
+            pictureBoxParking.Height);
+            for (int i = 0; i < countLevel; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + (i + 1));
+            }
+            listBoxLevels.SelectedIndex = 0;
+        }
+
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width,
+            pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking[listBoxLevels.SelectedIndex].Draw(gr);
+                pictureBoxParking.Image = bmp;
+            }
+        }
+
+        private void buttonSetLocomotive_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    var train = new TrainVehicle(100, 1000, dialog.Color);
+                    int place = parking[listBoxLevels.SelectedIndex] + train;
+                    if (place == -1)
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        logger.Error("Нет свободных мест");
+                    }
+                    Draw();
+                }
+            }
+        }
+
+        private void buttonTakeTrain_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                if (maskedTextBoxPlace.Text != "")
+                {
+                    try
+                    {
+                        var train = parking[listBoxLevels.SelectedIndex] -
+                       Convert.ToInt32(maskedTextBoxPlace.Text);
+                        Bitmap bmp = new Bitmap(pictureBoxTakeTrain.Width,
+                       pictureBoxTakeTrain.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        train.SetPosition(5, 5, pictureBoxTakeTrain.Width,
+                       pictureBoxTakeTrain.Height);
+                        train.DrawTrain(gr);
+                        pictureBoxTakeTrain.Image = bmp;
+                        logger.Info("Изъят автомобиль " + train.ToString() + " с места "
+                       + maskedTextBoxPlace.Text);
+                        Draw();
+                    }
+                    catch (ParkingNotFoundException ex)
+                    {
+                        MessageBox.Show(ex.Message, "Не найдено", MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                        logger.Error("Не найдено");
+                        Bitmap bmp = new Bitmap(pictureBoxTakeTrain.Width,
+                       pictureBoxTakeTrain.Height);
+                        pictureBoxTakeTrain.Image = bmp;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Неизвестная ошибка",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        logger.Error("Неизвестная ошибка");
+                    }
+                }
+            }
+        }
+
+        private void buttonSetElecTrain_Click(object sender, EventArgs e)
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == DialogResult.OK)
+                    {
+                        var train = new ElecTrain(100, 1000, dialog.Color, dialogDop.Color,
+                       true, true);
+                        int place = parking[listBoxLevels.SelectedIndex] + train;
+                        if (place == -1)
+                        {
+                            MessageBox.Show("Нет свободных мест", "Ошибка",
+                           MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            logger.Error("Нет свободных мест");
+                        }
+                        Draw();
+                    }
+                }
+            }
+        }
+
+        private void listBoxLevels_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void AddTrain(ITransport train)
+        {
+            if (train != null && listBoxLevels.SelectedIndex > -1)
+            {
+                try
+                {
+                    int place = parking[listBoxLevels.SelectedIndex] + train;
+                    logger.Info("Добавлен автомобиль " + train.ToString() + " на место "
+                   + place);
+                    Draw();
+                }
+                catch (ParkingOverflowException ex)
+                {
+                    MessageBox.Show(ex.Message, "Переполнение", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                    logger.Error("Переполнение");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Неизвестная ошибка",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    logger.Error("Неизвестная ошибка");
+                }
+            }
+        }
+        private void buttonFormTrainConfig_Click(object sender, EventArgs e)
+        {
+            trainForm = new FormTrainConfig();
+            trainForm.AddEvent(AddTrain);
+            trainForm.Show();
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    parking.SaveData(saveFileDialog.FileName);
+                    MessageBox.Show("Сохранение прошло успешно", "Результат",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    logger.Info("Сохранено в файл " + saveFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Неизвестная ошибка при сохранении",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    logger.Error("Неизвестная ошибка при сохранении");
+                }
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    parking.LoadData(openFileDialog.FileName);
+                    MessageBox.Show("Загрузили", "Результат", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                    logger.Info("Загружено из файла " + openFileDialog.FileName);
+                }
+                catch (ParkingOccupiedPlaceException ex)
+                {
+                    MessageBox.Show(ex.Message, "Занятое место", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                    logger.Error("Занятое место");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Неизвестная ошибка при сохранении",
+                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    logger.Error("Неизвестная ошибка при сохранении");
+                }
+                Draw();
+            }
+        }
+    }
+}
